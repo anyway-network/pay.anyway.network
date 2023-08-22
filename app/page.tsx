6f9b8f8748d8a9067a94bedaeb78c8831b75432e
gnehs <jayuiop@gmail.com>
2023-08-23 01:35:14 +0800

    updated animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { useAccount } from "wagmi";
 function Page() {
@@ -47,101 +47,98 @@ function Page() {
           <span className=" md:hidden">AnyWay</span>
         </div>
         <ConnectButton />
-      </div>{" "}
-      {step === "product" && (
-        <motion.div
-          initial={{ opacity: 0, y: 200 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="flex flex-col mx-auto w-[95vw] max-w-[512px] my-8"
-        >
-          <h1 className="text-4xl font-logo font-bold p-4 pb-2">Payment</h1>
-          <div className="flex flex-col bg-[#76c481] bg-opacity-10 rounded-2xl p-4 border border-[#76c481] border-opacity-20">
-            <motion.div className="bg-white p-8 flex items-center justify-center aspect-square rounded-lg border border-[#76c481] border-opacity-20">
-              <img
-                src="/iphone-compare-iphone-14-pro-202209.jpeg"
-                className="h-[300px]"
-                alt="product"
-              />
-            </motion.div>
-            <motion.h2
-              layout
-              layoutId="name"
-              className="text-2xl font-logo mt-3"
-            >
-              iPhone 15 Pro
-            </motion.h2>
-            <motion.h3 layout layoutId="price" className="font-logo mt-1">
-              $1,499.00 USDC
-            </motion.h3>
-            <ul className="mt-2 list-disc px-4">
-              <li className="font-bold">Demo</li>
-              <li>6.1-inch Super Retina XDR display</li>
-              <li>Ceramic Shield, tougher than any smartphone glass</li>
-              <li>
-                Advanced dual-camera system with 12MP Wide and Telephoto cameras
-              </li>
-              <li>Industry-leading IP68 water resistance</li>
-              <li>USB-C connector for charging and accessories</li>
-            </ul>
-            {!loading && (
-              <button
-                className="w-full bg-[#5b9763] hover:bg-opacity-80 active:bg-opacity-90 rounded-lg mt-3 p-3 text-white flex items-center justify-center gap-2"
-                onClick={() => getCallbackData()}
-              >
-                Pay with{" "}
-                <img
-                  src="/logo.svg"
-                  className="w-6 h-6 inline-block"
-                  alt="logo"
-                />{" "}
-                <span className="font-logo">AnyWay</span>
-              </button>
-            )}
-            {loading && (
-              <button className="w-full bg-[#5b9763] bg-opacity-80 rounded-lg mt-3 p-3 text-white flex items-center justify-center gap-2">
-                <div className="h-6 w-6 rounded-full border-2 border-white border-b-transparent animate-spin"></div>
-              </button>
-            )}
-          </div>
-        </motion.div>
-      )}
-      {step === "payment" && (
-        <motion.div
-          initial={{ opacity: 0, y: 200 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="flex flex-col mx-auto w-[95vw] max-w-[512px] my-8"
-        >
-          <h1 className="text-4xl font-logo font-bold p-4 pb-2">Payment</h1>
-          <div className="flex flex-col bg-[#76c481] bg-opacity-10 rounded-2xl p-4 border border-[#76c481] border-opacity-20">
-            <div className="flex gap-4 items-center">
-              <motion.div className="bg-white p-3 flex items-center justify-center rounded-lg border border-[#76c481] border-opacity-20 h-20 w-20">
+      </div>
+      <AnimatePresence mode="wait">
+        {step === "product" && (
+          <motion.div
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, x: -200 }}
+            transition={{ duration: 0.25 }}
+            className="flex flex-col mx-auto w-[95vw] max-w-[512px] my-8"
+            key={1}
+          >
+            <h1 className="text-4xl font-logo font-bold p-4 pb-2">Payment</h1>
+            <div className="flex flex-col bg-[#76c481] bg-opacity-10 rounded-2xl p-4 border border-[#76c481] border-opacity-20">
+              <motion.div className="bg-white p-8 flex items-center justify-center aspect-square rounded-lg border border-[#76c481] border-opacity-20">
                 <img
                   src="/iphone-compare-iphone-14-pro-202209.jpeg"
-                  className="h-full w-full object-contain"
+                  className="h-[300px]"
                   alt="product"
                 />
               </motion.div>
-              <div>
-                <motion.h2
-                  layout
-                  layoutId="name"
-                  className="text-2xl font-logo"
+              <motion.h2 className="text-2xl font-logo mt-3">
+                iPhone 15 Pro
+              </motion.h2>
+              <motion.h3 className="font-logo mt-1">$1.00 USDC</motion.h3>
+              <ul className="mt-2 list-disc px-4">
+                <li className="font-bold">Demo</li>
+                <li>6.1-inch Super Retina XDR display</li>
+                <li>Ceramic Shield, tougher than any smartphone glass</li>
+                <li>
+                  Advanced dual-camera system with 12MP Wide and Telephoto
+                  cameras
+                </li>
+                <li>Industry-leading IP68 water resistance</li>
+                <li>USB-C connector for charging and accessories</li>
+              </ul>
+              {!loading && (
+                <motion.button
+                  className="w-full bg-[#5b9763] hover:bg-opacity-80 active:bg-opacity-90 rounded-lg mt-3 p-3 text-white flex items-center justify-center gap-2"
+                  onClick={() => getCallbackData()}
                 >
-                  iPhone 15 Pro
-                </motion.h2>
-                <motion.h3 layout layoutId="price" className="font-logo mt-1">
-                  $1,499.00 USDC
-                </motion.h3>
-              </div>
+                  Pay with{" "}
+                  <img
+                    src="/logo.svg"
+                    className="w-6 h-6 inline-block"
+                    alt="logo"
+                  />{" "}
+                  <span className="font-logo">AnyWay</span>
+                </motion.button>
+              )}
+              {loading && (
+                <motion.button className="w-full bg-[#5b9763] bg-opacity-80 rounded-lg mt-3 p-3 text-white flex items-center justify-center gap-2">
+                  <div className="h-6 w-6 rounded-full border-2 border-white border-b-transparent animate-spin"></div>
+                </motion.button>
+              )}
             </div>
-            <button className="w-full bg-[#5b9763] hover:bg-opacity-80 active:bg-opacity-90 rounded-lg mt-3 p-3 text-white flex items-center justify-center gap-2">
-              Pay $1,000
-            </button>
-          </div>
-        </motion.div>
-      )}
+          </motion.div>
+        )}
+        {step === "payment" && (
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.25 }}
+            className="flex flex-col mx-auto w-[95vw] max-w-[512px] my-8"
+            key={2}
+          >
+            <h1 className="text-4xl font-logo font-bold p-4 pb-2">Payment</h1>
+            <div className="flex flex-col bg-[#76c481] bg-opacity-10 rounded-2xl p-4 border border-[#76c481] border-opacity-20">
+              <div className="flex gap-4 items-center">
+                <motion.div className="bg-white p-3 flex items-center justify-center rounded-lg border border-[#76c481] border-opacity-20 h-20 w-20">
+                  <img
+                    src="/iphone-compare-iphone-14-pro-202209.jpeg"
+                    className="h-full w-full object-contain"
+                    alt="product"
+                  />
+                </motion.div>
+                <div>
+                  <motion.h2 className="text-2xl font-logo">
+                    iPhone 15 Pro
+                  </motion.h2>
+                  <motion.h3 className="font-logo mt-1">$1.00 USDC</motion.h3>
+                </div>
+              </div>
+              <motion.button className="w-full bg-[#5b9763] hover:bg-opacity-80 active:bg-opacity-90 rounded-lg mt-3 p-3 text-white flex items-center justify-center gap-2">
+                Pay $1,000
+              </motion.button>
+              <p className="text-sm text-[#5b9763] mt-2 opacity-50">
+                Any transaction fees will be deducted from the payment amount.
+              </p>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
