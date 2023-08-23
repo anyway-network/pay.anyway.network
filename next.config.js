b0fe69582efa70fc7986cb51963d58a152cdf99c
gnehs <jayuiop@gmail.com>
2023-08-23 20:14:09 +0800

    added deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: config => {
+  output: "export",
+  trailingSlash: true,
+  distDir: "dist",
+  webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
 };
